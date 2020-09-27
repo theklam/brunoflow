2e8da680dadfd6014061d63d121f9c3197a8cde1
Kalvin Lam <kalvin_lam@brown.edu>
2020-09-27 16:22:47 -0700

    lazy exec'

--- ad/node.py
+++ ad/node.py
@@ -69,6 +69,7 @@ class Node:
         # once i have calculated all of the values that run into the neighbors,
         # then i can take this values to construct me. 
         in_vals = [a.val if isinstance(a, Node) else a for a in self.inputs]
+        print(self.forward_func)
         self.val = self.forward_func(*in_vals)
         self.grad = np.zeros_like(self.val,dtype=np.float64)
 
